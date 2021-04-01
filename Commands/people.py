0ebdb32840fc25a1d8f0fb7bb5c45c87ab782266
Kevvinn <kevinhu229@gmail.com>
2021-04-01 11:38:34 -0700

    add rnb

---
+++ Commands/people.py
@@ -0,0 +1,20 @@
+import discord
+import os, random
+from discord.ext import commands
+
+class People(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(name="sunny", aliases = ["sexy"])
+    async def _sunny(self, ctx):
+        f = discord.File('./resources/sunny/' + random.choice(os.listdir('./resources/sunny/')))
+        await ctx.send(f"Invoked by {ctx.message.author.name}",file=f)
+    
+    @commands.command(name="rakenbake", aliases = ["rnb"])
+    async def _sunny(self, ctx):
+        f = discord.File('./resources/rnb/' + random.choice(os.listdir('./resources/rnb/')))
+        await ctx.send(f"Invoked by {ctx.message.author.name}",file=f)
+
+def setup(client):
+    client.add_cog(People(client))
